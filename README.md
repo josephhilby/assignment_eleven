1ff8afde24fa02a0a8f46224dfeb620580eb6a56
Joseph Hilby <joseph.hilby@gmail.com>
2025-11-27 09:49:53 -0600

    assignment complete

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 1. Run MongoDB on your local machine.
-  - docker run -d -p 27017:27017 mongo
+  docker run -d -p 27017:27017 mongo
+
 2. Start App and set up tracing
-  - node --require tracing.js index.js
+  node --require ./tracing.js index.js
+
 3. Hit the API
-  - curl http://localhost:3000/todos
+  curl http://localhost:3000/todos
+
+4. Start Jagger
+  docker run -d --name jaeger \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 5778:5778 \
+  -p 9411:9411 \
+  jaegertracing/jaeger:2.5.0
+
+5. Check Jaegger UI at http://localhost:16686
--- tracing.js
+++ tracing.js
@@ -3,30 +3,41 @@ const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 
+// Instrumentations
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} = require('@opentelemetry/sdk-metrics');
-
-//Instrumentations
 const { ExpressInstrumentation } =
   require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } =
   require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
+// TRACE Exporter
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const otlpTraceExplorer = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
+
+// METRIC Exporter (not used by Jagger but included for completeness)
+//  must be aggregated in a reader before export
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const otlpMetricExporter = new OTLPMetricExporter({
+  url: 'http://localhost:4318/v1/metrics',
+});
+
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: otlpMetricExporter,
+  concurrencyLimit: 1,
+});
+
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: "todo-service"
   }),
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
+  traceExporter: otlpTraceExplorer,
+  metricReader: metricReader,
   instrumentations: [
     getNodeAutoInstrumentations(),
     new ExpressInstrumentation(),
